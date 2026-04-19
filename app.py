99474c6b797354c4fafb5fbfa278f51a9e91db25
csyhlim <caseyhlimx@gmail.com>
2026-04-19 15:41:23 +0700

    update UI + add copy button + fix routing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, jsonify
 from service import shorten_url, get_url
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -25,6 +26,21 @@ def redirect_url(code):
 def list_urls():
     from storage import load_data
     return jsonify(load_data())
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+@app.route("/shorten-ui", methods=["POST"])
+def shorten_ui():
+    url = request.form.get("url")
+
+    try:
+        code = shorten_url(url)
+        short_url = f"http://127.0.0.1:5000/{code}"
+        return render_template("index.html", short_url=short_url)
+    except:
+        return render_template("index.html", error="Invalid URL")
 
 if __name__ == "__main__":
     app.run(debug=True)
